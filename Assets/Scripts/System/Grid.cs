71a444b0c2444e6e4d7e7efdc7f54ce961bf7772
Neti Phochot <103875653+netiphoc@users.noreply.github.com>
2022-12-27 23:17:14 +0700

    [add] Grid system

---
+++ Assets/Scripts/System/Grid.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace System
+{
+    public class Grid<TElement> where TElement : MonoBehaviour
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public float CellSize { get; }
+
+        private TElement[,] _elements;
+
+        public Grid(int width, int height, float cellSize)
+        {
+            Width = width;
+            Height = height;
+            CellSize = cellSize;
+
+            _elements = new TElement[width, height];
+        }
+
+        public TElement GetElementAt(int x, int y)
+        {
+            if (x < 0 || x >= Width || y < 0 || y >= Height)
+            {
+                return null;
+            }
+
+            return _elements[x, y];
+        }
+
+        public TElement GetElementAt(float x, float y)
+        {
+            return GetElementAt(Mathf.FloorToInt(x), Mathf.FloorToInt(y));
+        }
+
+        public void SetElementAt(int x, int y, TElement element)
+        {
+            _elements[x, y] = element;
+        }
+    }
+}
